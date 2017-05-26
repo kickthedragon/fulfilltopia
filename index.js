e1d0158686c41fe7657ffe5e93c7f0281b133df8
Eric Remigino <eric.remigino@gmail.com>
2017-05-26 16:56:00 -0400

    adding addorder method

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+'use strict';
 import xmlrpc from 'xmlrpc';
+import types from './lib/types';
+
+var fftopiaSDK = module.exports = function(rhu, username, password, handler) {
+	this.username = username;
+	this.password = password;
+	this.responseHandler = handler || function() {};
+	this.client = xmlrpc.createSecureClient('https://' + rhu + '.veracore.com');
+};
+
+fftopiaSDK.prototype.methodCaller = function(service, data, callback) {
+	var cb = (typeof(callback)) === 'function' ? callback : this.responseHandler;
+	this.client.methodCall(service, data, cb);
+};
+
+fftopiaSDK.prototype.addOrder = function(cMap, callback) {
+	var ca = [this.username, this.password, cMap];
+	this.methodCaller('AddOrder', ca, callback);
+};
+
+fftopiaSDK.types = types;
