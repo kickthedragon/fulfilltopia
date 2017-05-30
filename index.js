90d15dcbeeeb11571ca2c7d862a1550dcfb7c2c5
Eric Remigino <eric.remigino@gmail.com>
2017-05-30 14:21:40 -0400

    another test with soap

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-var xmlrpc = require('xmlrpc');
-var types = require('./lib/types');
+var soap = require('soap');
 
 var fftopiaSDK = module.exports = function(rhu, username, password, handler) {
 	this.username = username;
 	this.password = password;
 	this.responseHandler = handler || function() {};
-	this.client = xmlrpc.createSecureClient('https://' + rhu + '.veracore.com');
+	this.client = soap.createClient('https://' + rhu + '.veracore.com');
+	this.client.setSecurity(new soap.BasicAuthSecurity(this.username, this.password));
 };
 
-fftopiaSDK.prototype.methodCaller = function(service, data, callback) {
-	var cb = (typeof(callback)) === 'function' ? callback : this.responseHandler;
-	this.client.methodCall(service, data, cb);
-};
+// fftopiaSDK.prototype.methodCaller = function(service, data, callback) {
+// 	var cb = (typeof(callback)) === 'function' ? callback : this.responseHandler;
+// 	this.client.methodCall(service, data, cb);
+// };
 
 fftopiaSDK.prototype.addOrder = function(cMap, callback) {
-	var ca = [this.username, this.password, cMap];
-	this.methodCaller('AddOrder', ca, callback);
+	//var ca = [this.username, this.password, cMap];
+	this.client.AddOrder(cMap, callback);
 };
-
-fftopiaSDK.types = types;
