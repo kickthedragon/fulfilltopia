fb4bd62fa4afdd4c06431db2e1c42bf3841883f3
Eric Remigino <eric.remigino@gmail.com>
2017-06-01 13:47:00 -0400

    final piece attempt to get this working with strong soap

--- index.js
+++ index.js
@@ -5,18 +5,22 @@ var fftopiaSDK = module.exports = function(rhu, username, password, handler) {
 	this.username = username;
 	this.password = password;
 	this.responseHandler = handler || function() {};
-	soap.createClient('https://' + rhu + '.veracore.com/pmomsws/oms.asmx?WSDL', (err, client) => {
+	var options = {
+		envelopeKey: 'soapenv',
+	};
+	soap.createClient('https://' + rhu + '.veracore.com/pmomsws/oms.asmx?WSDL', options, (err, client) => {
 		client.setSecurity(new soap.BasicAuthSecurity(this.username, this.password));
+		var authHeaders = {
+			AuthenticationHeader: {
+				Username: this.username,
+				Password: this.password
+			}
+		};
+		client.addSoapHeader(authHeaders);
 		this.client = client;
 	});
 };
 
-// fftopiaSDK.prototype.methodCaller = function(service, data, callback) {
-// 	var cb = (typeof(callback)) === 'function' ? callback : this.responseHandler;
-// 	this.client.methodCall(service, data, cb);
-// };
-
 fftopiaSDK.prototype.addOrder = function(cMap, callback) {
-	//var ca = [this.username, this.password, cMap];
 	this.client.AddOrder(cMap, callback);
 };
