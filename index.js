01b18ca994e1cb468534e25d6063dc6b7051c6e1
Eric Remigino <eric.remigino@gmail.com>
2017-06-22 17:13:05 -0400

    returning all info

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ fftopiaSDK.prototype.getOrder = function(orderId, callback) {
 	}, (err, request, body) => {
 		if (!err) {
 			var json = parser.toJson(body);
-			return callback(err, JSON.parse(json)['soap:Envelope']['soap:Body']['GetOrderInfoResponse']['GetOrderInfoResult']['OrdHead']);
+			return callback(err, JSON.parse(json)['soap:Envelope']['soap:Body']['GetOrderInfoResponse']['GetOrderInfoResult']);
 		} else {
 			return callback(err, request);
 		}
